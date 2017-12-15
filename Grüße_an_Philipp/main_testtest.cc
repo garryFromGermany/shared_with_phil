574ce268194fb2c451f131425136efcf089ac3e9
Garrett May <garrettmay@live.de>
2017-12-15 17:15:37 +0100

    main_vorlage gelöscht und Grüße an philipp erstellt

--- Grüße_an_Philipp/main_testtest.cc
+++ Grüße_an_Philipp/main_testtest.cc
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
+#include "testtest_funktion.h"
 
 int main(){
-
+testtest();
 return 0;
 }
---
+++ Grüße_an_Philipp/testtest_funktion.cc
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+void testtest(){
+cout << "Grüße an Philipp" << endl;
+}
